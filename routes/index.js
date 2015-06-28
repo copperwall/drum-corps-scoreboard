abbe888bf94af7027a6cfeafdbced5557b40c7e5
Chris Opperwall <chriso@ifixit.com>
2015-06-28 19:13:16 -0400

    Only show the three most recent competitions

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var Promise = require('promise');
 var score_scraper = require('../score_scraper');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
    score_scraper.getEventList().then(function(list) {
-      return score_scraper.getAllCompetitions(list);
+      var competitions = list.competitions.slice(0,3);
+      return Promise.all(competitions.map(score_scraper.getCompetition));
    }).then(function(competitions) {
      res.render('index', { competitions: competitions });
    });
